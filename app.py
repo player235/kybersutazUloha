872e999c20f0c83acdb7187f66a183fc6404db90
kyber <kyber>
2022-02-27 22:24:20 +0100

    add basic auth

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 """Flask App Project."""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, request, redirect, url_for
 app = Flask(__name__)
 
 FLAG = os.environ[‘flag’]
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def flag():
-    return "<p>" + FLAG + "</p>"
-       
+    if request.method == 'POST':
+        if request.form['password'] == "6044fac47c2c688a213fe26dcbe3bbdd15be29b083bee0777c4d81b050373a22":
+            return "<p>" + FLAG + "</p>"
+        else:
+            error = 'Invalid username/password'
+    return """<form action="/" method="POST">
+  <label for="fname">User:</label><br>
+  <input type="text" id="fname" name="fname" value="Admin"><br>
+  <label for="lname">Password:</label><br>
+  <input type="text" id="password" name="password"><br><br>
+  <input type="submit" value="Submit">
+</form> """
 
 
 if __name__ == '__main__':
